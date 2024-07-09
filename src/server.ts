24fd0a35f911a1a854ba1bad8ff031e129faa70e
Gustavo Campagnin <gcampagnin@gmail.com>
2024-07-09 18:19:16 -0300

    feat: add get links route

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import { confirmParticipant } from "./routes/confirm-participant";
 import { createActivity } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activities";
 import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
 
 const app = fastify()
 
@@ -23,6 +24,7 @@ app.register(confirmParticipant)
 app.register(createActivity)
 app.register(getActivities)
 app.register(createLink)
+app.register(getLinks)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server running! 🚀')
